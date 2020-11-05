0b4a2e14e6391f7b91295a4ad335717b79a0b91d
daspete <daspetemail@gmail.com>
2020-11-06 00:04:38 +0100

    fix: fixed missing files and directories in package.json

--- test/frontend/index.js
+++ test/frontend/index.js
@@ -1,4 +1,7 @@
 const Frontend = require('manablox-frontend')
+const nuxt = require('nuxt')
+
+console.log(nuxt.Generator)
 
 const serverConfig = require('./config/server')
 const clientConfig = require('./config/client')
